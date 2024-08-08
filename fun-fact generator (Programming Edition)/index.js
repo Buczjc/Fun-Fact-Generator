9b196ac92238f2116fab558833485720cab59c1a
BuczJc <jc.buclares15@gmail.com>
2024-08-08 18:18:03 +0800

    added componenets

--- fun-fact generator (Programming Edition)/index.js
+++ fun-fact generator (Programming Edition)/index.js
@@ -1,6 +1,23 @@
-let msg = "fuck you";
+
 $(document).ready(function () {
   $("#addMsg").on("click", function () {
-    $(".container").append("<p>" + msg + "</p>");
+    loadAPI();
   });
 });
+
+function loadAPI() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET",'fun-facts.json',true);
+
+  xhr.onload = function() {
+    if(xhr.status==200) {
+      console.log("Successfully fetched..");
+      var fun_facts = JSON.parse(xhr.responseText);
+      $(".container").append("<p>" + fun_facts[3].facts + "/<p>")
+    }else {
+      console.log("Failed.." + xhr.status);
+    }
+  }
+  xhr.send();
+
+}
