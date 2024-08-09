a83b1811795c332225dd7990cfc71b370cb4861e
Buczjc <jc.buclares15@gmail.com>
2024-08-09 22:39:30 +0800

    final production

--- fun-fact generator (Programming Edition)/index.js
+++ fun-fact generator (Programming Edition)/index.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
   $("#addMsg").on("click", function () {
     loadAPI();
@@ -7,17 +6,28 @@ $(document).ready(function () {
 
 function loadAPI() {
   var xhr = new XMLHttpRequest();
-  xhr.open("GET",'fun-facts.json',true);
+  xhr.open("GET", "fun-facts.json", true);
 
-  xhr.onload = function() {
-    if(xhr.status==200) {
+  xhr.onload = function () {
+    if (xhr.status == 200) {
       console.log("Successfully fetched..");
       var fun_facts = JSON.parse(xhr.responseText);
-      $(".container").append("<p>" + fun_facts[3].facts + "/<p>")
-    }else {
-      console.log("Failed.." + xhr.status);
+      var randomFact = fun_facts[Math.floor(Math.random() * fun_facts.length)];
+      $(".container").empty();
+      $(".container").append(
+        '<div class="quote-container">' +
+          "<h1>" +
+          randomFact.facts_about +
+          "</h1>" +
+          "<p>" +
+          randomFact.facts +
+          "</p>" +
+          "</div>"
+      );
+      $(".container_p").hide();
+    } else {
+      alert("Failed.. " + xhr.status);
     }
-  }
+  };
   xhr.send();
-
 }
